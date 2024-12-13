3fd1c1f4276d9ae909a2b07a26cd277d20bded4c
greensnapback0229 <smdmim@gmail.com>
2024-12-13 10:00:00 +0900

    Init

---
+++ src/main/java/greensnaback0229/pr_review_server/PrReviewServerApplication.java
@@ -0,0 +1,13 @@
+package greensnaback0229.pr_review_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrReviewServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/greensnaback0229/pr_review_server/PrReviewServerApplicationTests.java
@@ -0,0 +1,13 @@
+package greensnaback0229.pr_review_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
